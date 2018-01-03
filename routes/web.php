5d08429545887388de4d38c0bf4058ee9a2d6c03
Jaco Oversluizen <jhoversluizen@gmail.com>
2018-01-03 21:27:26 +0100

    inbetween episode 9 and 10

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,16 @@
 <?php
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
