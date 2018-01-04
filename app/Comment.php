a8bbc4080ded0a6c8c8b00d684bc6ebcda0b68b0
Jaco Oversluizen <jhoversluizen@gmail.com>
2018-01-04 22:30:04 +0100

    Episode 18 Associate with users

--- app/Comment.php
+++ app/Comment.php
@@ -8,6 +8,10 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-//
 }
