ef8dcab81a23b640fefc08e67ed0ed085e6c67ea
Jaco Oversluizen <jhoversluizen@gmail.com>
2018-01-14 18:12:17 +0100

    Episode 28 Form Requests and Form Objects

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Mail\Welcome;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
 {
@@ -13,26 +11,11 @@ class RegistrationController extends Controller
         return view('registration.create');
     }
 
-    public function store()
+    public function store(RegistrationForm $form)
     {
-        // Validate the form
-        $this->validate(request(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|confirmed'
-        ]);
 
         // Create and save the user
-        $user = User::create([
-            'name' => request('name'),
-            'email' => request('email'),
-            'password' => bcrypt(request('password'))
-        ]);
-
-        // Sign them in
-        auth()->login($user);
-
-        \Mail::to($user)->send(new Welcome($user));
+        $form->persist();
 
         // Redirect to the home page
         return redirect()->home();
